9e16f59322ba6f7f5fb656d72b4437db081338d7
prajogotio <ggkgaya@hotmail.com>
2016-10-05 13:27:19 +0800

    Populate Author and PublicationAuthor tables

--- sql/populate.sql
+++ sql/populate.sql
@@ -1,7 +1,7 @@
-/* Populate publication table using publication.csv */
+-- Populate publication table using publication.csv.
 COPY Publication(category, key, mdate, publtype, reviewid, rating, title, booktitle, pages, year, address, journal, volume, number, month, school, chapter) FROM '/Users/prajogotio/proj/cz4031/dbstuffsocool31/dblp_xml_parser/publication.csv' CSV;
 
-/* Load publisher.csv into temporary table */
+-- Load publisher.csv into temporary table.
 DROP TABLE IF EXISTS PublisherCSV;
 CREATE TEMP TABLE PublisherCSV (
   publication_key TEXT NOT NULL,
@@ -9,12 +9,12 @@ CREATE TEMP TABLE PublisherCSV (
 );
 COPY PublisherCSV FROM '/Users/prajogotio/proj/cz4031/dbstuffsocool31/dblp_xml_parser/publisher.csv' CSV;
 
-/* Populate the publisher table */
+-- Populate the publisher table.
 DELETE FROM Publisher;
 INSERT INTO Publisher(name)
 SELECT DISTINCT publisher_name FROM PublisherCSV;
 
-/* Update the publisher information in publication table */
+-- Update the publisher information in publication table.
 UPDATE Publication SET publisher_id = temp.publisher_id
 FROM (
 SELECT pub2.publication_id pub_id, p.publisher_id publisher_id FROM Publication pub2
@@ -23,6 +23,30 @@ JOIN Publisher p ON pcsv.publisher_name = p.name
 ) temp
 WHERE publication_id = temp.pub_id;
 
-/* The last thing to do. This will take a very long time.
-ALTER TABLE Publication ADD UNIQUE (key);
-*/
+DROP TABLE PublisherCSV;
+
+
+-- Load author.csv.
+DROP TABLE IF EXISTS AuthorCSV;
+CREATE TEMP TABLE AuthorCSV (
+  publication_key TEXT NOT NULL,
+  author_name TEXT NOT NULL
+);
+COPY AuthorCSV FROM '/Users/prajogotio/proj/cz4031/dbstuffsocool31/dblp_xml_parser/author.csv' CSV;
+
+-- Populate Author table. Assumption: author names are unique.
+DELETE FROM Author;
+EXPLAIN ANALYZE
+INSERT INTO Author(name)
+SELECT DISTINCT author_name FROM AuthorCSV;
+
+-- Populate PublicationAuthor table.
+DELETE FROM PublicationAuthor;
+EXPLAIN ANALYZE
+INSERT INTO PublicationAuthor
+SELECT DISTINCT pub.publication_id, a.author_id
+FROM Author a
+JOIN AuthorCSV acsv ON a.name = acsv.author_name
+JOIN Publication pub ON pub.key = acsv.publication_key;
+
+DROP TABLE AuthorCSV;
